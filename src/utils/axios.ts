88d0b551b411ed79b7f7167b5ce812884cd85f8f
jinhee kim <wlsgml0229@naver.com>
2023-04-11 14:04:58 +0900

    axios 설정 추가 및 토큰설정 추가

---
+++ src/utils/axios.ts
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import { token } from './token';
+
+/**
+ * 기본 URL 설정
+ */
+// axios.defaults.baseURL = process.env.VUE_APP_CODEF_DOMAIN;
+
+/**
+ * axios 응답 인터셉터 설정
+ */
+export const customAxios = {
+  init() {
+    console.log('[http][setting start]', axios.defaults.baseURL);
+    axios.interceptors.response.use(
+      (res) => {
+        //서버 기본 응답 성공시
+        return res;
+      },
+      async (error) => {
+        token.remove();
+        this.setHeader();
+        this.removeHeader();
+        // router.push('/').catch(() => {});
+        return Promise.reject(error);
+      },
+    );
+  },
+  /**
+   * 토큰관련 셋팅
+   */
+  setHeader() {
+    axios.defaults.headers.common['Authorization'] = token.access()
+      ? token.access()
+      : '';
+    axios.defaults.headers.post['Content-Type'] =
+      'application/json;charset=UTF-8';
+  },
+  removeHeader() {
+    delete axios.defaults.headers.common['Authorization'];
+  },
+  /**
+   * Axios 관련 설정 / api 메소드
+   */
+
+  get(resource: any, params: any) {
+    return axios.get(`${resource}`, { params });
+  },
+  getBlobFile(resource: any, params: any) {
+    return axios.get(`${resource}`, { responseType: 'blob', params });
+  },
+  post(resource: any, args: any) {
+    return axios.post(`${resource}`, args);
+  },
+  update(resource: any, params: any) {
+    return axios.put(`${resource}`, params);
+  },
+  patch(resource: any, params: any) {
+    return axios.patch(`${resource}`, params);
+  },
+  delete(resource: string, params: any) {
+    return axios.delete(resource, { data: params });
+  },
+};
+
+export default customAxios;
---
+++ src/utils/token.ts
@@ -0,0 +1,17 @@
+const TOKEN_KEY = 'my_app_token';
+type tokenType = {
+  access: Function;
+  set: Function;
+  remove: Function;
+};
+export const token: tokenType = {
+  access: () => {
+    return localStorage.getItem(TOKEN_KEY);
+  },
+  set: (token: string) => {
+    localStorage.setItem(TOKEN_KEY, token);
+  },
+  remove: () => {
+    localStorage.removeItem(TOKEN_KEY);
+  },
+};
