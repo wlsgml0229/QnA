57142425645a1d901416b42a428a322d6f5125c4
jinhee kim <wlsgml0229@naver.com>
2023-04-16 16:42:59 +0900

    폰트 추가

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -4,13 +4,28 @@ import reset from 'styled-reset';
 export const GlobalStyles = createGlobalStyle` 
   ${reset}
 
+  @font-face { /* cafe24 폰트 적용 */
+    font-family: "Cafe24Air";
+    src: url("/assets/font/Cafe24SsurroundAir.ttf") format("truetype");
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'S-CoreDream-3Light';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   * {
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     box-sizing: border-box;
+    font-family: 'S-CoreDream-3Light',serif;
     }
   
   html {
+    font-family: 'S-CoreDream-3Light',serif;
       font-size: 62.5%;
     }
   
