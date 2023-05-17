c85685bb46e826dc72e49f27e623431ddb73852a
jinhee kim <wlsgml0229@naver.com>
2023-05-18 01:04:57 +0900

    로그인 화면 css 수정

--- src/login/LoginWrap.tsx
+++ src/login/LoginWrap.tsx
@@ -1,11 +1,17 @@
 import React, { useCallback } from 'react';
 import kakao_login_btn from '@assets/icon/kakao_sm_login.png';
-import naver_login_btn from '@assets/icon/naver_sm_login.png'
-import clip_logo from '@assets/icon/paperclip.gif'
+import naver_login_btn from '@assets/icon/naver_sm_login.png';
+import clip_logo from '@assets/icon/paperclip.gif';
 import { REST_API_KEY, REDIRECT_URI } from '../../secretConfig';
 import useSWR from 'swr';
 import Image from 'next/image';
-import { LoginContent, LoginWrapper, SNSBtnWrapper, SNSLoginWrapper, TitleWrapper } from './style';
+import {
+  LoginContent,
+  LoginWrapper,
+  SNSBtnWrapper,
+  SNSLoginWrapper,
+  TitleWrapper,
+} from './style';
 export const LoginWrap = () => {
   const onSubmit = useCallback(
     (e: React.MouseEvent<HTMLButtonElement>): void => {
@@ -20,37 +26,34 @@ export const LoginWrap = () => {
   return (
     <LoginWrapper>
       <LoginContent>
-      <TitleWrapper>
-        <p>
-          기록하고, 질문하고, 연결되는 <br/>
-          소셜 개발자 플랫폼
-        </p>
-        <h1>Clips</h1>
-        <Image
-          src={clip_logo}
-          alt='clip logo image'
-        />
-      </TitleWrapper>
-      <SNSLoginWrapper>
-      <p>SNS 계정으로 시작하기</p>
-      <SNSBtnWrapper>
-        <Image
-          src={kakao_login_btn} 
-          alt="kakao login"
-          onClick={loginHandler}
-        />
-        <Image
-          src={naver_login_btn} 
-          alt="naver login"
-          onClick={loginHandler}
-        />
-        <Image
-          src={naver_login_btn} 
-          alt="sns login"
-          onClick={loginHandler}
-        />
-      </SNSBtnWrapper>
-      </SNSLoginWrapper>
+        <TitleWrapper>
+          <p>
+            기록하고, 질문하고, 연결되는 <br />
+            소셜 개발자 플랫폼
+          </p>
+          <h1>Clips</h1>
+          <Image src={clip_logo} alt="clip logo image" />
+        </TitleWrapper>
+        <SNSLoginWrapper>
+          <p>SNS 계정으로 빠르게 시작하기</p>
+          <SNSBtnWrapper>
+            <Image
+              src={kakao_login_btn}
+              alt="kakao login"
+              onClick={loginHandler}
+            />
+            <Image
+              src={naver_login_btn}
+              alt="naver login"
+              onClick={loginHandler}
+            />
+            <Image
+              src={naver_login_btn}
+              alt="sns login"
+              onClick={loginHandler}
+            />
+          </SNSBtnWrapper>
+        </SNSLoginWrapper>
       </LoginContent>
     </LoginWrapper>
   );
