7721a1bdd854241f448c71b16d206b4b826b82b6
jinhee kim <wlsgml0229@naver.com>
2023-08-28 23:44:56 +0900

    feat: Update base date for the calendar component

--- src/main/components/CalendarGraph.tsx
+++ src/main/components/CalendarGraph.tsx
@@ -1,18 +1,32 @@
 import CalendarHeatmap from 'react-calendar-heatmap';
 import 'react-calendar-heatmap/dist/styles.css';
 import { CalendarGraphwrap } from '@src/main/style';
-export const CalendarGraph = () => {
+import { IWriteDate } from '../../../types';
+import { useState } from 'react';
+
+interface CalendarGraphProps {
+  dateList: IWriteDate[];
+}
+
+export const CalendarGraph = ({ dateList }: CalendarGraphProps) => {
+  const today = new Date();
+  const oneYearAgo = new Date(today);
+  oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+  const currentDate = today.toISOString().slice(0, 10);
+  const oneYearAgoDate = oneYearAgo.toISOString().slice(0, 10);
+
   return (
     <CalendarGraphwrap>
       <CalendarHeatmap
-        startDate={new Date('2023-01-01')}
-        endDate={new Date('2024-01-01')}
-        values={[
-          { date: '2016-01-01', count: 12 },
-          { date: '2016-01-22', count: 122 },
-          { date: '2016-01-30', count: 38 },
-          // ...and so on
-        ]}
+        startDate={new Date(oneYearAgoDate)}
+        endDate={new Date(currentDate)}
+        values={dateList}
+        classForValue={(value) => {
+          if (!value) {
+            return 'color-empty';
+          }
+          return `color-scale-${value.count}`;
+        }}
       />
     </CalendarGraphwrap>
   );
